99c6348b7b84c138342330b93972d946a78db39d
ludson96 <ludson_ps27@hotmail.com>
2026-05-12 14:58:01 -0300

    feat: :sparkles: create bottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Clone app Google Maps',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.blue)),
       home: const HomePage(),
     );
   }
--- lib/pages/home/home.page.dart
+++ lib/pages/home/home.page.dart
@@ -1,10 +1,79 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  final Completer<GoogleMapController> _mapController =
+      Completer<GoogleMapController>();
+
+  final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  int _selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      // body: GoogleMap(
+      //   mapType: MapType.normal,
+      //   initialCameraPosition: _kGooglePlex,
+      //   onMapCreated: (GoogleMapController controller) {
+      //     _mapController.complete(controller);
+      //   },
+      // ),
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          iconTheme: WidgetStateProperty.resolveWith((states) {
+            if (states.contains(WidgetState.selected)) {
+              return IconThemeData(color: Colors.blue[200]);
+            }
+            return const IconThemeData(color: Colors.grey);
+          }),
+        ),
+        child: NavigationBar(
+          backgroundColor: Color(0xFF151515),
+          indicatorColor: Colors.blue.withValues(alpha: 0.1),
+          labelTextStyle: WidgetStateProperty.resolveWith((states) {
+            if (states.contains(WidgetState.selected)) {
+              return const TextStyle(color: Colors.white);
+            }
+            return const TextStyle(color: Colors.grey);
+          }),
+          selectedIndex: _selectedIndex,
+          onDestinationSelected: (int index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          destinations: const <Widget>[
+            NavigationDestination(
+              icon: Icon(Icons.place_outlined),
+              selectedIcon: Icon(Icons.place),
+              label: 'Explorar',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.bookmark_border),
+              selectedIcon: Icon(Icons.bookmark),
+              label: 'Salvos',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.add_circle_outline),
+              selectedIcon: Icon(Icons.add_circle),
+              label: 'Contribuir',
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
